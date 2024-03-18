8bfa818931497f1f610d39ff0f230df2d5225a29
Agneshka2011 <157572959+Agneshka2011@users.noreply.github.com>
2024-03-18 15:17:41 +0000

    i comited this file

---
+++ named.py
@@ -0,0 +1,7 @@
+word='apple'
+new_word=''
+for x in word:
+    
+    if word==new_word:
+        print('palindrome')
+    else:
--- pry.py
+++ pry.py
@@ -19,6 +19,6 @@ print ('a3 is b3:', a3 is b3)
 a4 = 5
 b4 = 5
 print('a4 == b4:', a4 == b4)  # a4 == b4: True
-print('a4 is b4:', a4 is b4)  # a4 is b4: True
+print('a4 is b4:', a4+0 is b4+0)  # a4 is b4: True
 print('a4+1 is b4+1:', a4+1 is b4+1)  # a4+1 is b4+1: False
 
